87514605d3253bb47e9441dbad961bcebc1028df
Your Name <mrmofd@gmail.com>
2022-04-04 19:43:33 +0700

    animation2

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -1,23 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
+
     const tl = new TimelineMax()
-    const mySplitText = new SplitText('.heading',{type: 'chars'})
-    const chars = mySplitText.chars
+    // let mySplitText = new SplitText('.heading',{type: 'words'})
+    // let words = mySplitText.words
 
         tl.from('.information', 1, {
             x: '100%'
         })
 
-        tl.from('.left-menu-fixed', .5, {
+        tl.from('.left-menu-fixed', .6, {
             x: -50,
             opacity: 0
         })
 
-        tl.staggerFrom(chars, .45, {opacity: 0,}, .1)
-        tl.staggerFrom(chars, .45, {fontSize: 64})
+        tl.from('.right', .5, {
+            x: 100,
+            opacity: 0
+        } )
 
 
-});
+        // tl.staggerFrom(words, .45, {opacity: 0}, .1,)
+        //    .staggerFrom(words, .45, {fontSize: 64})
 
+        tl.from('.heading', .35, {
+            y: -100,
+            opacity: 0,
+        })
+
+        tl.duration(2)
+})
 
